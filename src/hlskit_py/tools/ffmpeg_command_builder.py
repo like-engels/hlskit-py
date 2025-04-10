2de308c036a4672a9e0ee6584b79c12b35af19dd
Engels Tercero <75184282+like-engels@users.noreply.github.com>
2025-04-09 22:51:47 -0600

    [General / CI] Update license and workflow files (#2)
    * feat: update license and workflow files
    
    * feat: add sample pytest test

--- src/hlskit_py/tools/ffmpeg_command_builder.py
+++ src/hlskit_py/tools/ffmpeg_command_builder.py
@@ -1,5 +1,4 @@
 class FfmpegCommandBuilder:
-
     @classmethod
     def build_simple_hls(
         cls,
@@ -9,20 +8,27 @@ class FfmpegCommandBuilder:
         preset: str,
         segment_filename: str,
         playlist_filename: str,
-        hls_time: int = 10
+        hls_time: int = 10,
     ) -> list[str]:
-
         command = [
             "ffmpeg",
-            "-i", "pipe:0",
-            "-vf", f"scale={width}:{height}",
-            "-c:v", "libx264",
-            "-crf", str(crf),
-            "-preset", preset,
-            "-hls_time", str(hls_time),
-            "-hls_playlist_type", "vod",
-            "-hls_segment_filename", segment_filename,
-            playlist_filename
+            "-i",
+            "pipe:0",
+            "-vf",
+            f"scale={width}:{height}",
+            "-c:v",
+            "libx264",
+            "-crf",
+            str(crf),
+            "-preset",
+            preset,
+            "-hls_time",
+            str(hls_time),
+            "-hls_playlist_type",
+            "vod",
+            "-hls_segment_filename",
+            segment_filename,
+            playlist_filename,
         ]
-        
+
         return command
